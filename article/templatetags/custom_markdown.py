a1b6366d25515fe0a925b5d0c3f8cf9d998ecd52
ButteredCat <xiao.yc@foxmail.com>
2015-08-29 20:46:26 +0800

    move from markdown to markdown2.

--- article/templatetags/custom_markdown.py
+++ article/templatetags/custom_markdown.py
@@ -1,8 +1,8 @@
-import markdown
+import markdown2
 
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.utils.encoding import force_text
+from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -11,7 +11,13 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def custom_markdown(value):
-    return mark_safe(markdown.markdown(value,
-                    extensions = ['markdown.extensions.fenced_code',
-                                'markdown.extensions.codehilite'],
+    '''
+    extensions = ['markdown.extensions.fenced_code',
+                  'markdown.extensions.codehilite']
+    return mark_safe(markdown.markdown(force_unicode(value), extensions,
                     safe_mode=True, enable_attributes=False))
+    '''
+    return mark_safe(markdown2.markdown(force_unicode(value),
+                                        extras=['code-friendly',
+                                        'fenced-code-blocks'])
+                                        )
