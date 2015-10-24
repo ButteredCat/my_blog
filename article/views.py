51b81b2cd5f443cab09c5de2679c7375ae540df7
ButteredCat <xiao.yc@foxmail.com>
2015-10-24 17:47:57 +0800

    Article.published.all() in views.py

--- article/views.py
+++ article/views.py
@@ -7,12 +7,10 @@ from .models import Article
 
 articles_per_page = 5
 
-def get_published_posts():
-    return Article.objects.exclude(is_draft=True)
 
 # Create your views here.
 def home(request):
-    posts = get_published_posts()
+    posts = Article.published.all()
     paginator = Paginator(posts, articles_per_page)
     page = request.GET.get('page')
     try:
@@ -25,13 +23,13 @@ def home(request):
 
 def detail(request, id):
     try:
-        post = get_published_posts().get(id=str(id))
+        post = Article.published.all().get(id=str(id))
     except:
         raise Http404
     return render(request, 'post.html', {'post': post})
 
 def archives(request):
-    posts = get_published_posts()
+    posts = Article.published.all()
     return render(request, 'archives.html', {'post_list': posts,
                                              'error': False})
 
@@ -40,7 +38,7 @@ def about(request):
 
 def search_tag(request, tag) :
     try:
-        post_list = get_published_posts().filter(category__iexact=tag)
+        post_list = Article.published.all().filter(category__iexact=tag)
     except Article.DoesNotExist :
         raise Http404
     return render(request, 'tag.html', {'post_list': post_list})
@@ -51,7 +49,7 @@ def search(request):
         if not s:
             return render(request, 'home.html')
         else:
-            posts = get_published_posts().filter(title__icontains=s)
+            posts = Article.published.all().filter(title__icontains=s)
             return render(request, 'archives.html', {'post_list': posts,
                                                'error': len(posts)==0})
     return redirect('/')
