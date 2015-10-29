8e2d9ec5a9a9e26536b832ce91b1fe7a10cb026f
ButteredCat <xiao.yc@foxmail.com>
2015-10-29 14:04:14 +0800

    pack to deploy.

--- pack.py
+++ pack.py
@@ -4,19 +4,33 @@ import os
 
 SITE_PACKAGES = 'site-packages/'
 BUNDLES = 'bundles/'
-LIBS = (
-        BUNDLES+'django-pagedown/pagedown/',
-
-
+APPS = (
+        ('pagedown', 'django-pagedown/pagedown/'),
+        ('markdown2.py', 'python-markdown2/lib/markdown2.py'),
+        ('django_admin_bootstrapped',
+            'django-admin-bootstrapped/django_admin_bootstrapped/'),
+        ('solo', 'django-solo/solo/'),
        )
+APP_NAMES = [EACH_APP[0] for EACH_APP in APPS]
+LIBS = [BUNDLES + EACH_APP[1] for EACH_APP in APPS]
 
-def main():
+def prepare():
     for each in LIBS:
         os.system('cp -r %s %s' % (each, SITE_PACKAGES))
+
+def package():
     os.system('tar -zcv --exclude-from .tarignore -f ../deploy.tar.gz \
                 -C %s .' % (os.getcwd()))
-    os.system('rm -rf %s' % (SITE_PACKAGES + 'pagedown'))
+    
+def cleanup():
+    for each in APP_NAMES:
+        os.system('rm -rf %s' % (SITE_PACKAGES + each))
 
+def main():
+    prepare()
+    package()
+    cleanup()
 
 if __name__=='__main__':
     main()
+
