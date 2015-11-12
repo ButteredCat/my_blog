1d351d91a9eff5e2d6298c85a3a7142b611c07bd
ButteredCat <yunchen.xiao@gmail.com>
2015-11-12 17:15:55 +0800

    add test cases.

--- article/tests.py
+++ article/tests.py
@@ -6,13 +6,15 @@ from django.utils import timezone
 
 from .models import Article, Category
 
+
 def create_category(name):
     return Category.objects.create(name=name)
 
 def get_category_by_name(name):
     return Category.objects.get(name=name)
 
-def create_article(title, days, is_draft=False, category_name='test', content='default'):
+def create_article(title, days, content='default', category_name='test',
+        is_draft=False):
     pub_time = timezone.now() + datetime.timedelta(days=days) 
     try:
         category = get_category_by_name(category_name)
@@ -27,9 +29,8 @@ class ArticleDetailViewTests(TestCase):
     draft = None
 
     def setUp(self):
-        self.published = create_article('published article', -1, False, 'test',
-                'test article')
-        self.draft = create_article('draft', -1, True)
+        self.published = create_article('published article', -1, 'test article')
+        self.draft = create_article('draft', -1, 'editing', 'test', True)
         
     def test_should_have_access_to_published(self):
         response = self.client.get(reverse('detail', args=(self.published.id,)))
@@ -51,3 +52,40 @@ class ArticleDetailViewTests(TestCase):
         response = self.client.get(reverse('detail', args=(self.draft.id,)))
         self.assertEqual(response.status_code, 404)
 
+
+class ArticleCategoryViewTests(TestCase):
+    category1 = []
+    category_name = 'catgory_one'
+
+    def setUp(self):
+        for i in range(5):
+            title = 'article %d' % i
+            self.category1.append(create_article(title, -i, 'default',
+                self.category_name))
+
+    def test_should_get_404_when_category_does_not_exist(self):
+        response = self.client.get(reverse('search_category',
+            args=('notexist',)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_should_get_article_when_access_category(self):
+        response = self.client.get(reverse('search_category',
+            args=(self.category_name,)))
+        for i in range(len(self.category1)):
+            self.assertContains(response, self.category1[i].title)
+
+    def test_should_not_get_draft(self):
+        draft = create_article('draft', -1, 'nothing', self.category_name, True)
+        self.category1.append(draft)
+        response = self.client.get(reverse('search_category',
+            args=(self.category_name,)))
+        self.assertNotContains(response, draft.title)
+
+class ArticleArchiveViewTests(TestCase):
+    def setUp(self):
+        pass
+
+
+class ArticleSearchViewTests(TestCase):
+    def setUp(self):
+        pass
