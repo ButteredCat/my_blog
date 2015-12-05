efe3e1b91b8df98d195b40aaf705abed1f24af16
ButteredCat <yunchen.xiao@gmail.com>
2015-12-05 19:42:06 +0800

    category count bug fixed.

--- article/custom_processors.py
+++ article/custom_processors.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from .models import Category
 
 def category(request):
-    category = Category.objects.annotate(num_article=Count('article'))
+    category = Category.objects.filter(article__is_draft=False).annotate(
+            num_article=Count('article'))
     return {'categories': category,}
     
