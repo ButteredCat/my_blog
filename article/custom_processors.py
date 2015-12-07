1608bb7ac75b7c6dbd0a6daf44e382082ade6720
ButteredCat <yunchen.xiao@gmail.com>
2015-12-07 11:26:03 +0800

    month archive sidebar.

--- article/custom_processors.py
+++ article/custom_processors.py
@@ -14,7 +14,11 @@ def month_list(request):
     year_month = Set()
     for each in articles:
         year_month.add((each.date_time.year, each.date_time.month))
-    year_month = list(year_month)
-    year_month.sort(reverse=True)
-    return {'year_month': year_month,}
-
+    counter = {}.fromkeys(year_month, 0)    # counter = {(year, month):0, ... }
+    for each in articles:
+        counter[(each.date_time.year, each.date_time.month)] += 1
+    year_month_counter = []
+    for key in counter:
+        year_month_counter.append([key[0], key[1], counter[key]]) 
+    year_month_counter.sort(reverse=True)   # {[year, month, counter], ... }
+    return {'year_month_counter': year_month_counter,}
